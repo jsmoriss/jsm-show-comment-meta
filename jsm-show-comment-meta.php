5429bf0bd15d17df2b6865f911cfea26ce2d3791
JS Morisset <jsm@surniaulula.com>
2021-06-28 06:25:26 -0700

    v1.0.0

--- jsm-show-comment-meta.php
+++ jsm-show-comment-meta.php
@@ -8,7 +8,7 @@
  * Author: JS Morisset
  * Author URI: https://surniaulula.com/
  * License: GPLv3
- * License URI: http://www.gnu.org/licenses/gpl.txt
+ * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Show all comment meta (aka custom fields) in a metabox on comment editing pages.
  * Requires PHP: 7.0
  * Requires At Least: 4.5
