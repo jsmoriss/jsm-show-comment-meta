c74130f24003d8b8320d62208f6b52485f00db05
JS Morisset <jsm@surniaulula.com>
2021-09-09 05:08:55 -0700

    v1.0.0

--- jsm-show-comment-meta.php
+++ jsm-show-comment-meta.php
@@ -12,7 +12,7 @@
  * Description: Show all comment meta (aka custom fields) in a metabox on comment editing pages.
  * Requires PHP: 7.0
  * Requires At Least: 5.0
- * Tested Up To: 5.8
+ * Tested Up To: 5.8.1
  * Version: 1.0.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
