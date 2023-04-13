8d1aa8badc0494caee9143b36628f740a7168ede
JS Morisset <jsm@surniaulula.com>
2023-04-13 06:40:55 -0700

    v3.0.11

--- jsm-show-comment-meta.php
+++ jsm-show-comment-meta.php
@@ -11,7 +11,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Show comment metadata in a metabox when editing comments - a great tool for debugging issues with comment metadata.
  * Requires PHP: 7.2
- * Requires At Least: 5.4
+ * Requires At Least: 5.5
  * Tested Up To: 6.2.0
  * Version: 3.0.11
  *
