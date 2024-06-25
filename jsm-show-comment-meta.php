e522c6bdcc5b519182021e81e63f425e44bbbca5
jsmoriss <jsm@surniaulula.com>
2024-06-25 04:08:46 +0000

    v4.3.0

--- jsm-show-comment-meta.php
+++ jsm-show-comment-meta.php
@@ -12,7 +12,7 @@
  * Description: Show comment metadata in a metabox when editing comments - a great tool for debugging issues with comment metadata.
  * Requires PHP: 7.2.34
  * Requires At Least: 5.8
- * Tested Up To: 6.5.4
+ * Tested Up To: 6.5.5
  * Version: 4.3.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
