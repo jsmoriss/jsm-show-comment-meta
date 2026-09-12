1a3c0a5a4ce0f8cf937c7eeb15d23e7d3078b5ab
jsmoriss <jsm@surniaulula.com>
2026-09-12 14:34:22 +0000

    v4.9.1-rc.1

--- lib/comment.php
+++ lib/comment.php
@@ -93,7 +93,7 @@ if ( ! class_exists( 'JsmScmComment' ) ) {
 
 			$metabox_id   = 'jsmscm';
 			$obj_id       = SucomUtil::sanitize_int( $_POST[ 'obj_id' ] );		// Returns integer or null.
-			$meta_key     = SucomUtil::sanitize_meta_key( $_POST[ 'meta_key' ] );	// Decode/remove html/js/css and quotes.
+			$meta_key     = SucomUtil::sanitize_meta_key( $_POST[ 'meta_key' ] );	// Decode and remove html/js/css and quotes.
 			$table_row_id = SucomUtil::sanitize_key( $metabox_id . '_' . $obj_id . '_' . $meta_key );
 			$comment_obj  = get_comment( $obj_id );
 			$delete_cap   = apply_filters( 'jsmstm_delete_meta_capability', 'manage_options', $comment_obj );
